8594ae28570b7eebe74ea963e49ef7dcb00e1617
Jack Lowrey (RIT Student) <jgl5736@rit.edu>
2025-02-18 15:06:58 -0500

    Moved FSM logic to Controller

--- servoController.vhd
+++ servoController.vhd
@@ -13,10 +13,9 @@ ENTITY servoController IS
     clk        : IN std_logic;          -- 50 Mhz system clock
     reset_n    : IN std_logic;          -- active low system reset
     write      : IN std_logic;          -- active high write enable
-    address    : IN std_logic;  -- address of register to be written to (from CPU)
+    address    : IN std_logic;          -- address of register to be written to (from CPU)
     writedata  : IN std_logic_vector(31 DOWNTO 0);  -- data from the CPU to be stored in the component
     --
-    ext_addr_export  : IN  std_logic;  -- address of register to be read from (from other VHDL)
     out_wave_export  : OUT std_logic;  -- wave data visible to other components
     irq : OUT std_logic  -- signal to interrupt the processor                      
     );
@@ -24,6 +23,9 @@ END ENTITY servoController;
 
 ARCHITECTURE rtl OF servoController IS
 
+  signal period_cntr : std_logic_vector(25 downto 0);
+  signal angle_cntr : std_logic_vector(25 downto 0);
+  
   -- ram_type is a 2-dimensional array or inferred ram.  
   -- It stores eight 32-bit values
   TYPE ram_type IS ARRAY (1 DOWNTO 0) OF std_logic_vector (31 DOWNTO 0);
@@ -31,13 +33,13 @@ ARCHITECTURE rtl OF servoController IS
 
   --internal signal to address ram
   SIGNAL internal_addr : std_logic;  
-  SIGNAL ext_addr  : std_logic;  
-  SIGNAL ext_data  : std_logic;  
+  
+  type state_type is (SWEEP_RIGHT, INT_RIGHT, SWEEP_LEFT, INT_LEFT);
+  signal next_state : state_type;
+  signal current_state : state_type;
+  signal is_new_state : std_logic;
   
 BEGIN
-
-  ext_addr <= ext_addr_export;
-  out_wave_export <= ext_data;
 
   --this process loads data from the CPU.  The CPU provides the address, 
   --the data and the write enable signal
@@ -61,28 +63,54 @@ BEGIN
     IF (reset_n = '0') THEN
       internal_addr <= '0';
     ELSIF (clk'event AND clk = '1') THEN
-      internal_addr <= ext_addr;
+      internal_addr <= address;
     END IF;
   END PROCESS;
 
---- heartbeat counter --------
-  counter_proc : process (CLOCK_50) begin
-    if (rising_edge(CLOCK_50)) then
-      if (reset_n = '0') then
-        cntr <= "00" & x"000000";
-      else
-        cntr <= cntr + ("00" & x"000001");
-      end if;
-    end if;
-  end process counter_proc;
-  
-  --output the data being requested
-  ext_data <= Registers(conv_integer(internal_addr));  
-
-  --this process interrupts the processor if it receives and invalid signal
-  interrupts : PROCESS(invalid)
+-- Next State Logic
+  nsl : process(clk, reset_n)
   BEGIN
-    IF  THEN
+    IF (reset_n = '0') THEN
+      next_state <= SWEEP_RIGHT;
+    ELSIF (clk'event AND clk = '1') THEN
+      case (current_state) is
+        when SWEEP_RIGHT =>
+          if (angle_count >= max_angle_count) THEN
+            next_state <= INT_RIGHT;
+          ELSE
+            next_state <= SWEEP_RIGHT;
+          end if;
+        when INT_RIGHT =>
+          if (write ='1') THEN
+            next_state <= SWEEP_LEFT;
+          else 
+            next_state <= INT_RIGHT;
+          end IF;
+        when SWEEP_LEFT =>
+          if (angle_count <= min_angle_count) THEN
+            next_state <= INT_LEFT;
+          ELSE
+            next_state <= SWEEP_LEFT;
+          end if;
+        when INT_LEFT=>
+          if (write ='1') THEN
+            next_state <= SWEEP_RIGHT;
+          else 
+            next_state <= INT_LEFT;
+          end IF;
+      end case;
+    END IF;
+  end PROCESS;
+  
+  -- Write enable
+  fsm : process(current_state) IS
+  begin
+    
+
+  --this process interrupts the processor once a sweep is complete
+  interrupts : PROCESS(current_state)
+  BEGIN
+    IF (current_state == INT_RIGHT || current_state == INT_LEFT) THEN
       irq <= '1';
     ELSE
       irq <= '0';
--- servoSweep.vhd
+++ servoSweep.vhd
@@ -9,15 +9,17 @@ USE ieee.std_logic_unsigned.ALL;
 USE ieee.numeric_std.ALL;
 
 ENTITY servoSweep IS
-  PORT(CLOCK_50  : IN    std_logic;
-       KEY       : IN    std_logic_vector(0 DOWNTO 0);
-       SW        : IN    std_logic_vector(3 DOWNTO 0);
-       LEDR      : OUT   std_logic_vector(9 DOWNTO 0)
-       );
+  PORT( CLOCK_50  : IN    std_logic;
+        KEY       : IN    std_logic_vector(0 DOWNTO 0);
+        SW        : IN    std_logic_vector(3 DOWNTO 0);
+        LEDR      : OUT   std_logic_vector(9 DOWNTO 0)
+      );
 END ENTITY servoSweep;
 
 ARCHITECTURE rtl OF servoSweep IS
 
+  signal cntr : std_logic_vector(25 downto 0);
+  
   -- ram_type is a 2-dimensional array or inferred ram.  
   -- It stores eight 32-bit values
   TYPE ram_type IS ARRAY (1 DOWNTO 0) OF std_logic_vector (31 DOWNTO 0);
@@ -28,15 +30,7 @@ ARCHITECTURE rtl OF servoSweep IS
   signal current_state : state_type;
   signal is_new_state : std_logic;
 
-  --internal signal to address ram
-  SIGNAL internal_addr : std_logic;  
-  SIGNAL ext_addr  : std_logic;  
-  SIGNAL ext_data  : std_logic;  
-  
 BEGIN
-
-  ext_addr <= ext_addr_export;
-  out_wave_export <= ext_data;
 
   --this process loads data from the CPU.  The CPU provides the address, 
   --the data and the write enable signal
@@ -64,54 +58,7 @@ BEGIN
     END IF;
   END PROCESS;
 
--- Next State Logic
-  nsl : process(clk, reset_n)
-  BEGIN
-    IF (reset_n = '0') THEN
-      next_state <= SWEEP_RIGHT;
-    ELSIF (clk'event AND clk = '1') THEN
-      case (current_state) is
-        when SWEEP_RIGHT =>
-          if (angle_count >= max_angle_count) THEN
-            next_state <= INT_RIGHT;
-          ELSE
-            next_state <= SWEEP_RIGHT;
-          end if;
-        when INT_RIGHT =>
-          if (write ='1') THEN
-            next_state <= SWEEP_LEFT;
-          else 
-            next_state <= INT_RIGHT;
-          end IF;
-        when SWEEP_LEFT =>
-          if (angle_count <= min_angle_count) THEN
-            next_state <= INT_LEFT;
-          ELSE
-            next_state <= SWEEP_LEFT;
-          end if;
-        when INT_LEFT=>
-          if (write ='1') THEN
-            next_state <= SWEEP_RIGHT;
-          else 
-            next_state <= INT_LEFT;
-          end IF;
-      end case;
-    END IF;
-      
-  --output the data being requested
-  ext_data <= Registers(conv_integer(internal_addr));  
-
-  --this process interrupts the processor if it receives and invalid signal
-  interrupts : PROCESS(invalid)
-  BEGIN
-    IF  THEN
-      irq <= '1';
-    ELSE
-      irq <= '0';
-    END IF;
-  END PROCESS;
-
---- heartbeat counter --------
+  --- heartbeat counter --------
   counter_proc : process (CLOCK_50) begin
     if (rising_edge(CLOCK_50)) then
       if (reset_n = '0') then
